284982e4f398458ab08a8c6d32236823e9722014
Libby Miller <libby@nicecupoftea.org>
2018-07-31 22:11:47 +0100

    Adding the basic app stucture

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # piab_neue
-Podcast in a box for Radiodan Neue. Currently very minimal
+
+Podcast in a box for Radiodan Neue. Currently very minimal - just prints out messages. 
+More info on http://planb.nicecupoftea.org
---
+++ src/internal.js
@@ -0,0 +1,21 @@
+const appName = window.location.pathname.replace(/\//g, '');
+const websocket = createWebsocket();
+
+const main = async () => {
+  websocket.ready.then(() => {
+    console.log('Websocket connected');
+
+    websocket.publish({
+      topic: `${appName}/event/ready`,
+      payload: { msg: 'Internal app ready!' }
+    });
+  });
+
+  websocket.subscribe(new RegExp(`.*`), ({ topic, payload }) => {
+    console.log('Recieved message', topic, payload);
+  });
+
+  console.log('Internal app loaded');
+};
+
+main();
